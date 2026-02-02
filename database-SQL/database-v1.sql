7bc77b2a994cbdd99e153bd9ecd259da9a51e112
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2026-02-02 16:45:37 +0200

    add creation script for database

---
+++ database-SQL/database-v1.sql
@@ -0,0 +1,109 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.5.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for gradebook
+CREATE DATABASE IF NOT EXISTS `gradebook` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci */;
+USE `gradebook`;
+
+-- Dumping structure for table gradebook.assignment
+CREATE TABLE IF NOT EXISTS `assignment` (
+  `assignmentId` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `weight` float DEFAULT NULL,
+  `maxScore` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `gradeId` int(11) NOT NULL,
+  `courseId` int(11) NOT NULL,
+  PRIMARY KEY (`assignmentId`),
+  KEY `gradeId` (`gradeId`),
+  KEY `courseId` (`courseId`),
+  CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`gradeId`) REFERENCES `grade` (`gradeId`),
+  CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`courseId`) REFERENCES `course` (`courseId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table gradebook.course
+CREATE TABLE IF NOT EXISTS `course` (
+  `courseId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `teacherId` int(11) NOT NULL,
+  PRIMARY KEY (`courseId`),
+  KEY `teacherId` (`teacherId`),
+  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`teacherId`) REFERENCES `teacher` (`teacherId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table gradebook.enrollment
+CREATE TABLE IF NOT EXISTS `enrollment` (
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `enrollmentId` int(11) NOT NULL AUTO_INCREMENT,
+  `attendance` float DEFAULT NULL,
+  `finalGrade` int(11) DEFAULT NULL,
+  `studentId` int(11) NOT NULL,
+  `courseId` int(11) NOT NULL,
+  PRIMARY KEY (`enrollmentId`),
+  KEY `studentId` (`studentId`),
+  KEY `courseId` (`courseId`),
+  CONSTRAINT `enrollment_ibfk_1` FOREIGN KEY (`studentId`) REFERENCES `student` (`studentId`),
+  CONSTRAINT `enrollment_ibfk_2` FOREIGN KEY (`courseId`) REFERENCES `course` (`courseId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table gradebook.grade
+CREATE TABLE IF NOT EXISTS `grade` (
+  `gradeId` int(11) NOT NULL AUTO_INCREMENT,
+  `score` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `studentId` int(11) NOT NULL,
+  PRIMARY KEY (`gradeId`),
+  KEY `studentId` (`studentId`),
+  CONSTRAINT `grade_ibfk_1` FOREIGN KEY (`studentId`) REFERENCES `student` (`studentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table gradebook.student
+CREATE TABLE IF NOT EXISTS `student` (
+  `studentId` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `enrollmentYear` int(11) NOT NULL,
+  PRIMARY KEY (`studentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table gradebook.teacher
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `teacherId` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`teacherId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
