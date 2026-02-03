6ee39cd019f10016a9b6804a738076589c5b6de4
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2026-02-03 18:05:57 +0200

    add database connection

--- untitled/src/main/java/datasource/MariaDbConnection.java
+++ untitled/src/main/java/datasource/MariaDbConnection.java
@@ -1,4 +1,20 @@
 package datasource;
 
-public class mariaDbConnection {
+import jakarta.persistence.*;
+
+public class MariaDbConnection {
+
+    private static EntityManagerFactory emf = null;
+    private static EntityManager em = null;
+
+    public static EntityManager getInstance() {
+
+        if (em == null) {
+            if (emf == null) {
+                emf = Persistence.createEntityManagerFactory("GradebookMariaDbUnit");
+            }
+            em = emf.createEntityManager();
+        }
+        return em;
+    }
 }
