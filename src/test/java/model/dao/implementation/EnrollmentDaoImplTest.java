3702b7b2a6605e0729660c4927bec70b3f4f2f74
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2026-02-09 17:45:25 +0200

    save unfinished enrollment DAO test

---
+++ src/test/java/model/dao/implementation/EnrollmentDaoImplTest.java
@@ -0,0 +1,46 @@
+package model.dao.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnrollmentDaoImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByStudentId() {
+    }
+
+    @Test
+    void findByCourseId() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
