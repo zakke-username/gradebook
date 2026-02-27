60e9bdc40cf386c6eb3fa1a6bcf28c2690afe22c
Artem <artemda@metropolia.fi>
2026-02-27 21:24:53 +0200

    created login

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -7,10 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
-    public void start(Stage window) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
-        Parent root = loader.load();
-        window.setScene(new Scene(root));
-        window.show();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/LoginView.fxml"));
+        Scene loginScene = new Scene(loginLoader.load());
+        Stage loginStage = new Stage();
+        loginStage.setScene(loginScene);
+        loginStage.setTitle("Login");
+        loginStage.showAndWait();
+
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Parent root = mainLoader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Dashboard");
+        primaryStage.show();
     }
 }
