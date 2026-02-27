7afbe5acc7146889ce52190a801a397ab1545fc9
zakke-username <zakkezweyg@gmail.com>
2026-02-27 16:32:29 +0200

    new UI branch

---
+++ src/main/java/view/MainView.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+    public void start(Stage window) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Parent root = loader.load();
+        window.setScene(new Scene(root));
+        window.show();
+    }
+}
